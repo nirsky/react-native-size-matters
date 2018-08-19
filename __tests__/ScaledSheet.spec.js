dd0b6684c9e8e88eb74f53284b2ce67e95231489
Nir Hadassi <nir@soluto.com>
2018-08-19 10:54:22 +0300

    Handle negatives on ScaledSheet

--- __tests__/ScaledSheet.spec.js
+++ __tests__/ScaledSheet.spec.js
@@ -8,13 +8,16 @@ const input = {
         height: '50@vs',
         margin: {
             width: 12,
-            height: '12@s'
+            height: '12@s',
+            paddingBottom: -1
         }
     },
     row: {
         padding: '10@ms0.3',
         height: '34@ms',
-        margin: '0.5@ms0.9'
+        marginRight: '0.5@ms0.9',
+        marginLeft: '-0.5@ms0.9',
+        marginTop: '-10@s',
     }
 };
 
@@ -24,13 +27,16 @@ const expectedOutput = {
         height: verticalScale(50),
         margin: {
             width: 12,
-            height: scale(12)
+            height: scale(12),
+            paddingBottom: -1
         }
     },
     row: {
         padding: moderateScale(10, 0.3),
         height: moderateScale(34),
-        margin: moderateScale(0.5, 0.9)
+        marginRight: moderateScale(0.5, 0.9),
+        marginLeft: moderateScale(-0.5, 0.9),
+        marginTop: scale(-10),
     }
 };
 
