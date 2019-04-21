c84a48f5c729190e3f9df66046d75c7ce540bb66
Nir Hadassi <nir@soluto.com>
2019-04-21 17:34:34 +0300

    fixed extend types

--- extend.d.ts
+++ extend.d.ts
@@ -1,1 +1,14 @@
-export * from './index.d.ts';
+import * as RN from "react-native";
+import { StringifiedStyles, NamedStyles } from './index.d';
+
+declare module "react-native-size-matters/extend" {
+    export function scale(size: number): number;
+    export function verticalScale(size: number): number;
+    export function moderateScale(size: number, factor?: number): number;
+
+    export namespace ScaledSheet {
+        export function create<T extends NamedStyles<T> | NamedStyles<any>>(stylesObject: T): { 
+            [P in keyof T]: RN.RegisteredStyle<T[P] & Record<Extract<keyof T[P], keyof StringifiedStyles>, number>> 
+        };
+    }
+}
