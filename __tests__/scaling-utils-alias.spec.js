bbfc75ce398e528c5b1084adefb8a44f5db70e33
Nir Hadassi <nir.hadassi@gmail.com>
2019-12-05 18:06:32 +0200

    Version 0.3 features and improvements (#34)

---
+++ __tests__/scaling-utils-alias.spec.js
@@ -0,0 +1,25 @@
+jest.mock('react-native');
+import { s, vs, ms } from '..';
+
+describe('scaling-utils', () => {
+    test('scale returns the expected result based on mocked Dimensions', () => {
+        expect(s(2.5)).toBe(5);
+        expect(s(100)).toBe(200);
+        expect(s(200)).toBe(400);
+    });
+
+    test('verticalScale returns the expected result based on mocked Dimensions', () => {
+        expect(vs(5)).toBe(7.5);
+        expect(vs(100)).toBe(150);
+        expect(vs(200)).toBe(300);
+    });
+
+    test('moderateScale returns the expected result based on mocked Dimensions', () => {
+        expect(ms(100)).toBe(150);
+        expect(ms(100, 0.1)).toBe(110);
+        expect(ms(100, 0.3)).toBe(130);
+        expect(ms(100, 0.6)).toBe(160);
+        expect(ms(100, 0.9)).toBe(190);
+        expect(ms(100, 2)).toBe(300);
+    });
+})
--- extend.d.ts
+++ extend.d.ts
@@ -5,6 +5,9 @@ declare module "react-native-size-matters/extend" {
     export function scale(size: number): number;
     export function verticalScale(size: number): number;
     export function moderateScale(size: number, factor?: number): number;
+    export function s(size: number): number;
+    export function vs(size: number): number;
+    export function ms(size: number, factor?: number): number;
 
     export namespace ScaledSheet {
         export function create<T extends NamedStyles<T> | NamedStyles<any>>(stylesObject: T): { 
