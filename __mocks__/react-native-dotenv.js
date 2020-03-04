f51574179a53fc9fc6b0afa36da82def0c041d08
Nikola Knežević <knezevicdev@gmail.com>
2020-03-04 15:53:09 +0100

    Update mocks

--- __mocks__/react-native-dotenv.js
+++ __mocks__/react-native-dotenv.js
@@ -1,6 +1,7 @@
 const dotenv = {
     SIZE_MATTERS_BASE_WIDTH: 525,
-    SIZE_MATTERS_BASE_HEIGHT: 1020
+    SIZE_MATTERS_BASE_HEIGHT: 1020,
+    SIZE_MATTERS_DISABLED_DEVICES: 'PHONE'
 };
 
 module.exports = dotenv;
--- __mocks__/react-native.js
+++ __mocks__/react-native.js
@@ -2,6 +2,9 @@ const reactNative = {
     Dimensions: {
         get: () => ({width: 700, height: 1020})
     },
+    PixelRatio: {
+        get: () => 2
+    },
     StyleSheet: {
         create: x => x
     }
