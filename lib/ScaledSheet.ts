2975fac3cc45a7d64c3e4e454e7b4f9750b8e6da
nirsky <nir@aspecto.io>
2021-03-30 15:24:38 +0300

    types

--- lib/ScaledSheet.ts
+++ lib/ScaledSheet.ts
@@ -1,11 +1,41 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ViewStyle, TextStyle, ImageStyle, RegisteredStyle } from 'react-native';
 import deepMap from './deep-map';
 
 // Groups                     Size                   Func Factor
 //                             1                      2    3
 const validScaleSheetRegex = /^(\-?\d+(?:\.\d{1,3})?)@(mv?s(\d+(?:\.\d{1,2})?)?|s|vs)r?$/;
+export interface StringifiedStyles {
+    fontSize?: string | number;
+    letterSpacing?: string | number;
+    lineHeight?: string | number;
+    textShadowRadius?: string | number;
+    borderBottomLeftRadius?: string | number;
+    borderBottomRightRadius?: string | number;
+    borderTopLeftRadius?: string | number;
+    borderTopRightRadius?: string | number;
+    borderBottomWidth?: string | number;
+    borderTopWidth?: string | number;
+    borderRightWidth?: string | number;
+    borderLeftWidth?: string | number;
+    borderRadius?: string | number;
+    shadowRadius?: string | number;
+    borderWidth?: string | number;
+    aspectRatio?: string | number;
+    rotation?: string | number;
+    scaleX?: string | number;
+    scaleY?: string | number;
+    translateX?: string | number;
+    translateY?: string | number;
+}
 
-const scaleByAnnotation = (scale, verticalScale, moderateScale, moderateVerticalScale) => (value) => {
+type NamedStyles<T> = { [P in keyof T]: ViewStyle | TextStyle | ImageStyle | StringifiedStyles };
+
+const scaleByAnnotation = (
+    scale: Function,
+    verticalScale: Function,
+    moderateScale: Function,
+    moderateVerticalScale: Function
+) => (value: string) => {
     if (!validScaleSheetRegex.test(value)) {
         return value;
     }
@@ -16,12 +46,11 @@ const scaleByAnnotation = (scale, verticalScale, moderateScale, moderateVertical
     let scaleFunc = regexExecResult[2];
     const scaleFactor = regexExecResult[3]; // string or undefined
 
-    if (scaleFactor)
-        scaleFunc = scaleFunc.slice(0, - scaleFactor.length); // Remove the factor from it
+    if (scaleFactor) scaleFunc = scaleFunc.slice(0, -scaleFactor.length); // Remove the factor from it
 
     const shouldRound = value.endsWith('r');
 
-    let result;
+    let result: number;
 
     switch (scaleFunc) {
         case 's':
@@ -41,11 +70,18 @@ const scaleByAnnotation = (scale, verticalScale, moderateScale, moderateVertical
     return shouldRound ? Math.round(result) : result;
 };
 
-const scaledSheetCreator = (scale, verticalScale, moderateScale, moderateVerticalScale) => {
+const scaledSheetCreator = (
+    scale: Function,
+    verticalScale: Function,
+    moderateScale: Function,
+    moderateVerticalScale: Function
+) => {
     const scaleFunc = scaleByAnnotation(scale, verticalScale, moderateScale, moderateVerticalScale);
-    return {
-        create: (styleSheet: StyleSheet) => StyleSheet.create(deepMap(styleSheet, scaleFunc))
-    };
+    const create = <T extends NamedStyles<T> | NamedStyles<any>>(stylesObject: T) =>
+        StyleSheet.create(deepMap(stylesObject, scaleFunc)) as {
+            [P in keyof T]: RegisteredStyle<T[P] & Record<Extract<keyof T[P], keyof StringifiedStyles>, number>>;
+        };
+    return { create };
 };
 
 export default scaledSheetCreator;
--- lib/deep-map.ts
+++ lib/deep-map.ts
@@ -1,4 +1,4 @@
-const deepMap = (obj: any, fn: Function) => {
+const deepMap = (obj: any, fn: Function): any => {
     const deepMapper = (val: any) => (isObject(val)) ? deepMap(val, fn) : fn(val);
     if (Array.isArray(obj)) {
         return obj.map(deepMapper);
@@ -9,12 +9,12 @@ const deepMap = (obj: any, fn: Function) => {
     return obj;
 };
 
-const mapObject = (obj: any, fn: Function) => Object.keys(obj).reduce(
-    (res, key) => {
+const mapObject = (obj: any, fn: Function): any => Object.keys(obj).reduce(
+    (res: any, key: string) => {
         res[key] = fn(obj[key]);
         return res;
     }, {});
 
-const isObject = (myVar: any) => myVar && typeof myVar === 'object';
+const isObject = (myVar: any): boolean => myVar && typeof myVar === 'object';
 
 export default deepMap;
