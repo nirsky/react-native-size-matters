4764c0ae36fbbcf8a6f3c2939abb2ca9710a544b
Randall71 <51382079+Randall71@users.noreply.github.com>
2022-10-18 17:16:04 +0000

    chore: add StatusBar

--- examples/expo-example-app/App.js
+++ examples/expo-example-app/App.js
@@ -4,12 +4,15 @@ import FeedPage from "./components/Feed";
 import ChatPage from "./components/Chat";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
 
 const NativeStack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style="dark" />
+
       <NativeStack.Navigator initialRouteName="Home">
         <NativeStack.Screen name="Home" component={HomeScreen} />
         <NativeStack.Screen name="Chat" component={ChatPage} />
